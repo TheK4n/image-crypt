07c1282ae71b27c51c74881b61d5bfa2f1f7eedf
Pendosv <djvlad967891@gmail.com>
2021-05-29 23:02:05 +0300

    init

---
+++ Crypto.py
@@ -0,0 +1,93 @@
+
+from random import *
+from typing import *
+
+import numpy
+from PIL import Image, ImageDraw
+
+
+def get_random_list(length: int, key: Union[int, float, str]) -> list[int]:
+    """ """
+    seed(key)
+    lst = [i for i in range(length)]
+    shuffle(lst)
+    seed()
+    return lst
+
+
+def get_xy(array_: tuple[int, int], element: int) -> tuple[int, int]:
+    height, width = array_
+    a = numpy.array(range(0, height * width)).reshape((height, width))
+
+    index_y = element // width
+    index_x = list(a[index_y]).index(element)
+
+    return index_y, index_x
+
+
+def rgb_to_dec(rgb: tuple[int, int, int]) -> int:
+    r, g, b = rgb
+    return b * 65536 + g * 256 + r
+
+
+def get_encrypted_color(this_color: int, char: str) -> int:
+    this_char = ord(char)
+
+    # упаковка в RGB 323
+    new_color = (this_color & 0xF80000)  # 11111000 00000000 00000000
+    new_color |= (this_char & 0xE0) << 11  # 00000111 00000000 00000000
+    new_color |= (this_color & (0x3F << 10))  # 00000000 11111100 00000000
+    new_color |= (this_char & 0x18) << 5  # 00000000 00000011 00000000
+    new_color |= (this_color & (0x1F << 3))  # 00000000 00000000 11111000
+    new_color |= (this_char & 0x7)  # 00000000 00000000 00000111
+
+    return new_color
+
+
+def get_decrypted_char(new_color: int) -> str:
+    this_char = 0
+
+    # распаковка из RGB 323 обратно в байт
+    this_char |= (new_color & 0x70000) >> 11  # 00000111 00000000 00000000 -> 00000000 00000000 11100000
+    this_char |= (new_color & 0x300) >> 5  # 00000000 00000011 00000000 -> 00000000 00000000 00011000
+    this_char |= (new_color & 0x7)
+
+    return chr(this_char)
+
+
+def encrypt(image_name: str, msg: str, key: str):
+
+    msg = msg + '\0'
+
+    img = Image.open(image_name)
+    pix = img.load()
+    img_new = ImageDraw.Draw(img)
+
+    lst = get_random_list(img.size[0]*img.size[1], key)
+
+    gen_msg = (i for i in msg)
+    for i in lst:
+        coord = get_xy(img.size, i)
+        try:
+            img_new.point(coord, get_encrypted_color(rgb_to_dec(pix[coord]), next(gen_msg)))
+        except StopIteration:
+            break
+    img.save('result.bmp', 'BMP')
+
+
+def decrypt(image_name: str, key: str) -> str:
+    img = Image.open(image_name)
+    pix = img.load()
+    lst = get_random_list(img.size[0]*img.size[1], key)
+    msg = ''
+    for i in lst:
+        char = get_decrypted_char(rgb_to_dec(pix[get_xy(img.size, i)]))
+        if char == '\0':
+            break
+        msg += char
+    return msg
+
+
+if __name__ == '__main__':
+    encrypt('resources\\test.jpg', 'hello', key='key')
+    print(decrypt('result.bmp', key='key'))
---
+++ main.py
@@ -0,0 +1,58 @@
+import sys
+from Crypto import *
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
+import design
+
+
+class MainWindow(QMainWindow, design.Ui_MainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+
+        self.image_directory_dec = None
+        self.image_directory_enc = None
+
+        self.pushButton.clicked.connect(self.btn_load_image_clicked_enc)
+        self.pushButton_4.clicked.connect(self.encrypt_image)
+        self.pushButton_2.clicked.connect(self.btn_load_image_clicked_dec)
+        self.pushButton_3.clicked.connect(self.decrypt_image)
+
+    def btn_load_image_clicked_enc(self):
+        directory = QFileDialog.getOpenFileName(self, "Выберите картинку", filter='*.jpg *.bmp *.png')[0]
+
+        if directory != '':
+            self.image_directory_enc = directory
+            self.Debug_area_1.setText('Image to encrypt: ' + directory.split('/')[-1])
+
+    def encrypt_image(self):
+
+        encrypt(self.image_directory_enc, self.text_input.toPlainText(), self.lineEdit.text())
+        self.Debug_area_1.setText('Encrypted image saved as result.bmp')
+
+    def btn_load_image_clicked_dec(self):
+        directory = QFileDialog.getOpenFileName(self, "Выберите картинку", filter='*.jpg *.bmp *.png')[0]
+        if directory != '':
+            self.image_directory_dec = directory
+            self.Debug_area_2.setText('Image to decrypt: ' + directory.split('/')[-1])
+
+    def decrypt_image(self):
+        print(1)
+        res = decrypt(self.image_directory_dec, self.lineEdit_2.text())
+        self.text_output.setText(res)
+
+        self.Debug_area_2.setText('Successfully decrypted image')
+        if self.checkBox.isChecked():
+            with open('res.txt', 'w') as file:
+                file.write(res)
+
+
+def main():
+    app = QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = MainWindow()  # Создаём объект класса ExampleApp
+    window.show()  # Показываем окно
+    app.exec_()  # и запускаем приложение
+
+
+if __name__ == '__main__':
+    main()
