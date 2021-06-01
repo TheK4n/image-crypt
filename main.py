3093b5698ff8876a12ccf27ce8f414d734fca7c9
Pendosv <djvlad967891@gmail.com>
2021-06-01 23:44:54 +0300

    fix

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ class MainWindow(QMainWindow, design.Ui_MainWindow):
         try:
             encrypt(self.image_directory_enc, self.text_input.toPlainText(), self.lineEdit.text())
 
-            n = len(list(filter(lambda x: x.split('.')[-1] == 'bmp', list(os.listdir('results')))))
+            n = len(list(filter(lambda x: x.split('.')[-1] == 'bmp', list(listdir('results')))))
             self.Debug_area_1.setText(f'Encrypted image saved as encrypted_{n}.bmp')
         except MoreThanImgError:
             error_dialog = QMessageBox()
