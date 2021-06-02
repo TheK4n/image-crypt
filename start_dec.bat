8c2c5babb093a338679e6543fdd3ac2f7ba35e81
Pendosv <djvlad967891@gmail.com>
2021-06-02 16:46:09 +0300

    ref

---
+++ start_dec.bat
@@ -0,0 +1,4 @@
+@echo off
+echo python decrypt.py "results\encrypted_4.bmp" "key"
+python decrypt.py "results\encrypted_4.bmp" "key"
+pause
---
+++ start_enc.bat
@@ -0,0 +1,4 @@
+@echo off
+echo python encrypt.py "images\test.jpg" "Hello, World!" "key"
+python encrypt.py "images\test.jpg" "Hello, World!" "key"
+pause
