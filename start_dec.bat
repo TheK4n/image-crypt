294908b3b3a63e8e454cf04c67b03a6ac8ce405f
Pendosv <djvlad967891@gmail.com>
2021-06-02 22:59:56 +0300

    ref

--- start_dec.bat
+++ start_dec.bat
@@ -1,4 +1,4 @@
 @echo off
-echo python decrypt.py "results\encrypted_4.bmp" "key"
-python decrypt.py "results\encrypted_4.bmp" "key"
+echo python decrypt.py "results\encrypted_1.bmp" "key"
+python decrypt.py "results\encrypted_1.bmp" "key"
 pause
