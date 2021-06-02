9ffbba4c0eb39b35955ff2b51ea77069f0dc154a
Pendosv <djvlad967891@gmail.com>
2021-06-02 22:58:23 +0300

    ref

--- src/Crypto.py
+++ src/Crypto.py
@@ -1,7 +1,6 @@
-from typing import *
-
 from os import mkdir, listdir
 from random import seed, shuffle
+from typing import *
 
 from PIL import Image, ImageDraw
 
@@ -59,12 +58,12 @@ def get_encrypted_color(this_color: int, char: str) -> int:
         this_char -= 890
 
     # упаковка в RGB 323
-    new_color = (this_color & 0xF80000)       # 11111000 00000000 00000000
-    new_color |= (this_char & 0xE0) << 11     # 00000111 00000000 00000000
+    new_color = (this_color & 0xF80000)  # 11111000 00000000 00000000
+    new_color |= (this_char & 0xE0) << 11  # 00000111 00000000 00000000
     new_color |= (this_color & (0x3F << 10))  # 00000000 11111100 00000000
-    new_color |= (this_char & 0x18) << 5      # 00000000 00000011 00000000
-    new_color |= (this_color & (0x1F << 3))   # 00000000 00000000 11111000
-    new_color |= (this_char & 0x7)            # 00000000 00000000 00000111
+    new_color |= (this_char & 0x18) << 5  # 00000000 00000011 00000000
+    new_color |= (this_color & (0x1F << 3))  # 00000000 00000000 11111000
+    new_color |= (this_char & 0x7)  # 00000000 00000000 00000111
 
     return new_color
 
@@ -72,7 +71,7 @@ def get_encrypted_color(this_color: int, char: str) -> int:
 def get_decrypted_char(new_color: int) -> str:
     this_char = 0
     this_char |= (new_color & 0x70000) >> 11  # 00000111 00000000 00000000 -> 00000000 00000000 11100000
-    this_char |= (new_color & 0x300) >> 5     # 00000000 00000011 00000000 -> 00000000 00000000 00011000
+    this_char |= (new_color & 0x300) >> 5  # 00000000 00000011 00000000 -> 00000000 00000000 00011000
     this_char |= (new_color & 0x7)
 
     if this_char > 130:
@@ -82,14 +81,13 @@ def get_decrypted_char(new_color: int) -> str:
 
 
 def encrypt(image_name: str, msg: str, key: str):
-
     msg = msg + '\0'  # добавляет в конец сообщения как метку
 
     img = Image.open(image_name)
     pix = img.load()  # пиксели
     img_new = ImageDraw.Draw(img)
 
-    size = img.size[0]*img.size[1]
+    size = img.size[0] * img.size[1]
     if len(msg) > size:
         raise MoreThanImgError(f'Length of message <{len(msg)}> more than image <{size}>')
 
@@ -105,16 +103,16 @@ def encrypt(image_name: str, msg: str, key: str):
             break
     # кол-во файлов в формате bmp
     n = len(list(filter(lambda x: x.split('.')[-1] == 'bmp', list(listdir('results')))))
-    img.save(f'results\\encrypted_{n+1}.bmp', 'BMP')  # сохраняет зашифрованную картинку
+    img.save(f'results\\encrypted_{n + 1}.bmp', 'BMP')  # сохраняет зашифрованную картинку
 
 
 def decrypt(image_name: str, key: str) -> str:
     img = Image.open(image_name)
     pix = img.load()
-    matrix = get_matrix(*img.size)
 
+    matrix = get_matrix(*img.size)
     msg = ''
-    for i in get_random_list(img.size[0]*img.size[1], key):
+    for i in get_random_list(img.size[0] * img.size[1], key):
         char = get_decrypted_char(rgb_to_dec(pix[get_xy(matrix, i)]))
         if char == '\0':  # завершает цикл когда дошел до метки
             break
